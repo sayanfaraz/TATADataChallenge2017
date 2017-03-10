8d01d84a6efd1b9aee0a36b44de0cc83069cb0df
Lidia Injac <lidia.injac@mail.utoronto.ca>
2017-03-10 14:21:24 -0500

    Started work on main file

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from openpyxl import Workbook
+from openpyxl import load_workbook
+
 if __name__ == '__main__':
+    excel_data = Workbook()
+    excel_data = load_workbook("C:/Users/Owner/Documents/GitHub/TATADataChallenge2017/NBTC_Tata_Challenge.01.xlsx")
+    #excel_data.create_sheet("video_game_data", 0)
+    print(excel_data.get_sheet_names)
